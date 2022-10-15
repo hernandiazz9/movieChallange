8dd676382075125de6f0c6d1a08eda74225d74c1
Hernan <hernandiazz99@gmail.com>
2022-10-15 11:32:26 +0200

    t

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -10,5 +10,5 @@ export const axiosMovies = axios.create({
   //    "Content-Type": "multipart/form-data" 
   // }
   //   Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImlhdCI6MTY0NDE4MDY2MywiZXhwIjoxNjQ2NzcyNjYzfQ.7h8aw3VzO-yAfUpPRXZF2fMMlRY-E1eYkUs8CFEBSDE`
-  // }
+
 });
